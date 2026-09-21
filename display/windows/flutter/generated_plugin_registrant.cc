c0a5adbafb7f95ae3a50507fb0a9e7de73e0b434
jerin77777 <jerinjacob0007@gmai.com>
2026-09-21 13:01:37 +0530

    display fix

--- display/windows/flutter/generated_plugin_registrant.cc
+++ display/windows/flutter/generated_plugin_registrant.cc
@@ -6,13 +6,13 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <rive_common/rive_plugin.h>
+#include <rive_native/rive_native_plugin.h>
 #include <screen_retriever/screen_retriever_plugin.h>
 #include <window_manager/window_manager_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  RivePluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("RivePlugin"));
+  RiveNativePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("RiveNativePlugin"));
   ScreenRetrieverPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("ScreenRetrieverPlugin"));
   WindowManagerPluginRegisterWithRegistrar(
