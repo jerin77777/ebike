c161e505cead5279ef7800b845bd2df0a2590c6a
jerin77777 <jerinjacob0007@gmai.com>
2026-09-22 13:47:04 +0530

    display fix

--- display/lib/widgets.dart
+++ display/lib/widgets.dart
@@ -124,7 +124,7 @@ class _TurnIndicatorBarState extends State<TurnIndicatorBar>
                 // Bottom-right: Turn Indicator Neon Arrows
                 Positioned(
                   right: 12,
-                  bottom: 15,
+                  bottom: 20,
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
                     crossAxisAlignment: CrossAxisAlignment.center,
